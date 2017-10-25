b30fd2113dce7a24f79a1923a98b34faa5819f4d
Marcel Oertig <marcel_oertig@yahoo.de>
2017-10-25 20:05:43 +0200

    WPF übernimmt Stefan Spielerei mit NUnit

---
+++ BuisnesLogicWithTest/BuisnesLogic/Checks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnesLogic
+{
+    public class Checks
+    {
+      public bool hasCutSurface(int[] rectangle1, int[] rectangle2)
+      {
+         int[] resultRectangle = { Math.Max(rectangle1[0],rectangle2[0]),
+                                   Math.Min(rectangle1[1],rectangle2[1]),
+                                   Math.Min(rectangle1[2],rectangle2[2]),
+                                   Math.Max(rectangle1[3],rectangle2[3])};
+
+         if(resultRectangle[0] > resultRectangle[2] || resultRectangle[1] < resultRectangle[3])
+         {
+            return false;
+         }
+         else
+         {
+            return true;
+         }
+      }
+    }
+}
---
+++ BuisnesLogicWithTest/NUnitTest/TestClass.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuisnesLogic;
+
+namespace NUnitTest
+{
+   [TestFixture]
+   public class TestClass
+   {
+      [Test]
+      public void TestMethod()
+      {
+         // TODO: Add your test code here
+         Assert.Pass("Your first passing test");
+      }
+
+      [Test]
+      public void TestHasCutSurface()
+      {
+         Checks check = new Checks();
+
+
+         Assert.IsTrue(check.hasCutSurface(new int[] { 1, 4, 4, 1 }, new int[] { 3, 6, 6, 3 }));
+
+      }
+   }
+}
